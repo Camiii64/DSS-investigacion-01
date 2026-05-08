9bda18852ec75639871ece0c3a720e86a3e93312
Deaht-Bite-the-Pain <julian55apalomino@gmail.com>
2026-05-08 13:30:02 -0600

    feat: agregar landing page y dejar de trackear .env

--- hospital_management/tailwind.config.js
+++ hospital_management/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', 'system-ui', 'sans-serif'],
+        mono: ['"JetBrains Mono"', 'ui-monospace', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
